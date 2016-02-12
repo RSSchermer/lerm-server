b8d789851b38b50272c41672a18d5700e00f1ca6
R.S. Schermer <roland0507@gmail.com>
2016-02-12 12:27:41 +0100

    Add ProjectPolicy

---
+++ app/policies/project_policy.rb
@@ -0,0 +1,35 @@
+class ProjectPolicy < ApplicationPolicy
+  alias_attribute :project, :record
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    !user.nil?
+  end
+
+  def new?
+    create?
+  end
+
+  def manage?
+    !user.nil? && (user.super_admin? || user.project_member?(project))
+  end
+
+  def update?
+    manage?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    manage?
+  end
+end
