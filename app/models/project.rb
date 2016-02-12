607fcdf07b0d9a81930d5269a2b1a00ca6fd95f3
R.S. Schermer <roland0507@gmail.com>
2016-02-12 14:22:51 +0100

    Rename users association to members

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
-  has_many :users, through: :memberships
+  has_many :members, through: :memberships, source: :user
 
   validates :name, presence: true, uniqueness: true
 end
