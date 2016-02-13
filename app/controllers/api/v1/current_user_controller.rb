6c4f77764666edc131365881d4a081b0a3a74316
R.S. Schermer <roland0507@gmail.com>
2016-02-13 11:27:13 +0100

    Require token authentication for all current user actions

--- app/controllers/api/v1/current_user_controller.rb
+++ app/controllers/api/v1/current_user_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class CurrentUserController < BaseResourceController
-      before_action :doorkeeper_authorize!, except: [:index, :show, :get_related_resources, :show_relationship]
+      before_action :doorkeeper_authorize!
     end
   end
 end
