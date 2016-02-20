7bf8cdf93d7900a41aaacaa1a3609181e11d53f1
R.S. Schermer <roland0507@gmail.com>
2016-02-20 10:42:35 +0100

    Disallow index for all resources nested on a project

--- app/controllers/api/v1/rule_relationships_controller.rb
+++ app/controllers/api/v1/rule_relationships_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::RuleRelationshipsController < Api::V1::BaseResourceController
   before_action :doorkeeper_authorize!, except: [:index, :show, :get_related_resources, :show_relationship]
+
+  def index
+    head :unauthorized
+  end
 end
