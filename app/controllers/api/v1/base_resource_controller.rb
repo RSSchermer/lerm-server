810a5f4a72329cf1a13f0bc95f6fa22315ae3861
R.S. Schermer <roland0507@gmail.com>
2016-02-20 19:47:28 +0100

    Rescue from NotAuthorizedErrors with a 401 not authorized response

--- app/controllers/api/v1/base_resource_controller.rb
+++ app/controllers/api/v1/base_resource_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::BaseResourceController < ActionController::Base
   include JSONAPI::ActsAsResourceController
 
+  rescue_from Pundit::NotAuthorizedError, with: :reject_forbidden_request
+
   private
+
+  # TODO: this will be resolved in jsonapi-resources 0.7.1, remove this workaround when upgrading
+  def handle_exceptions(e)
+    if JSONAPI.configuration.exception_class_whitelist.any? { |k| e.class.ancestors.include?(k) }
+      raise e
+    else
+      super
+    end
+  end
+
+  def reject_forbidden_request
+    head :unauthorized
+  end
 
   def current_user
     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -1,3 +1,5 @@
 JSONAPI.configure do |config|
   config.operations_processor = :jsonapi_authorization
+
+  config.exception_class_whitelist = [Pundit::NotAuthorizedError]
 end
