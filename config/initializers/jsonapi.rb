1212ac4fb1b928e14c099169b30d6773c626e4cc
R.S. Schermer <roland0507@gmail.com>
2016-02-27 10:38:00 +0100

    Always include to one linkage data
    Workaround for Ember Data's current lack of support for eager-loading. Consider removing when Ember Data support improves.

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -2,4 +2,8 @@ JSONAPI.configure do |config|
   config.operations_processor = :jsonapi_authorization
 
   config.exception_class_whitelist = [Pundit::NotAuthorizedError]
+
+  # Workaround for current lack of include support in Ember Data
+  # TODO: remove when Ember Data eager-loading support improves
+  config.always_include_to_one_linkage_data = true
 end
