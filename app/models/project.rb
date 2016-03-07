04b003e8e141fe8840eb38985a28e4d89f61d226
R.S. Schermer <roland0507@gmail.com>
2016-03-07 12:25:45 +0100

    Add missing data elements relationship to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   has_many :members, through: :memberships, source: :user
   has_many :rules, dependent: :destroy
+  has_many :data_elements, dependent: :destroy
   has_many :rule_conflicts, dependent: :destroy
   has_many :rule_relationships, dependent: :destroy
 
